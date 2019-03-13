2e759331bc8ea976f6b883605f1bf785c83c8bd1
010473 <zhangxiaoting@htsc.com>
2019-03-14 00:27:54 +0800

    client注册lister观察者,以及回调处理的逻辑

--- app/src/main/java/com/demo/xzhangipcdemo/client/MainActivity.java
+++ app/src/main/java/com/demo/xzhangipcdemo/client/MainActivity.java
@@ -4,19 +4,23 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
 import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.OrientationHelper;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
 import com.demo.xzhangipcdemo.Book;
 import com.demo.xzhangipcdemo.IBookManagerInterface;
+import com.demo.xzhangipcdemo.IOnNewBookAvailableListener;
 import com.demo.xzhangipcdemo.R;
 import com.demo.xzhangipcdemo.server.BookManagerService;
 
@@ -30,6 +34,39 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private BookRecyclerViewAdapter mAdapter;
     private List<Book> mBooks = new ArrayList<>();
     private IBinder.DeathRecipient mDeathRecipient; //Binder连接断裂监听
+    private final static int MSG_NEW_BOOK_AVAILABLE = 0x1;
+    private ServiceConnection mServiceConnection;
+
+    //观察到server事件后的回调
+    private IOnNewBookAvailableListener mListener = new IOnNewBookAvailableListener.Stub() {
+        @Override
+        public void onNewBookAvailable(Book newBook) throws RemoteException {
+            Log.i("client", "onNewBookAvailable: " + newBook.toString());
+            mHandler.obtainMessage(MSG_NEW_BOOK_AVAILABLE, newBook).sendToTarget();
+        }
+    };
+
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case MSG_NEW_BOOK_AVAILABLE:
+                    Toast.makeText(MainActivity.this, "得到新书", Toast.LENGTH_SHORT).show();
+                    if (mBookManagerInterface != null) {
+                        try {
+                            mBooks = mBookManagerInterface.getBooks();
+                            mAdapter.updateBooks(mBooks);
+                        } catch (RemoteException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    break;
+                default:
+                    super.handleMessage(msg);
+
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,10 +100,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void bindRemoteService() {
-        ServiceConnection serviceConnection = new ServiceConnection() {
+        mServiceConnection = new ServiceConnection() {
             @Override
             public void onServiceConnected(ComponentName name, IBinder service) {
                 mBookManagerInterface = IBookManagerInterface.Stub.asInterface(service);
+                try {
+                    mBookManagerInterface.registerListener(mListener);
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
                 //给Binder设置死亡代理，Binder连接断裂时会回调其binderDied()
                 try {
                     service.linkToDeath(mDeathRecipient, 0);
@@ -80,7 +122,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 mBookManagerInterface = null;
             }
         };
-        this.bindService(new Intent(this, BookManagerService.class), serviceConnection, Context.BIND_AUTO_CREATE);
+        this.bindService(new Intent(this, BookManagerService.class), mServiceConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (mBookManagerInterface != null && mBookManagerInterface.asBinder().isBinderAlive()) {
+            try {
+                mBookManagerInterface.unregisterListener(mListener);
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+        }
+        if (mServiceConnection != null) {
+            this.unbindService(mServiceConnection);
+        }
+        super.onDestroy();
     }
 
     @Override
